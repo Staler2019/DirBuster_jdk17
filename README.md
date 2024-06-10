b112d8078332e75d8b56d52aa4e9356a4cef9184
Staler2019 <abcde121234@gmail.com>
2024-06-11 01:37:46 +0800

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# DirBuster jdk17
+
+Rebuild DirBuster v1.0RC1 on jdk17
+
+## Source Code
+
+https://sourceforge.net/projects/dirbuster/files/DirBuster%20Source/
---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+jpackage --name DirBuster --icon applet.icns --input . --app-version 1.0 --java-options -Xmx256M --main-jar DirBuster-1.0-RC1-src.jar
